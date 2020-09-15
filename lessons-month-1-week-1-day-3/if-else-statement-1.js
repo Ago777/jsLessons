e51c4cc8433cbca813e9c9e20e6acaf6f850a72a
Grigoryan <argishtigrigoryan1991@gmail.com>
2020-09-16 01:52:59 +0400

    asdfsdf

---
+++ lessons-month-1-week-1-day-3/if-else-statement-1.js
@@ -0,0 +1,48 @@
+// ---------COMPARE OPERATORS----------
+// >, <, ==, ===, >=, <=, !=,
+
+// let a = 10 > 7;
+// console.log(a);
+//
+// let b = 7 <= 7;
+// console.log(b);
+//
+// let c = '10' > 7;
+// console.log(c);
+//
+// let d = '10' > '2';
+// console.log(d);
+//
+// let e = 'some' > 5;
+// console.log(e);
+//
+// let str = 'some' > 'other';
+// console.log(str);
+//
+// let f = '5' > true;
+// console.log(f);
+//
+// let g = '' == false;
+// console.log(g);
+//
+// let h = 5 != 7;
+// console.log(h);
+
+// null == undefined and nothing else
+// let x = null == undefined;
+// console.log(x);
+
+// for >, <, >=, <= null--> 0, undefined--> NaN
+// let x = 0;
+// console.log(null >= x);
+
+//------------ HARD COMPARE ---------------
+
+// let x = 5;
+// let y = '5';
+// console.log(x == y);
+// console.log(x === y);
+
+
+
+
---
+++ lessons-month-1-week-1-day-3/strings-3.js
@@ -0,0 +1,45 @@
+// ----'STRING DATA TYPE'----
+
+// -------PROPERTY LENGTH-------
+
+// let string = 'some';
+// let stringLength = string.length;
+// console.log(stringLength);
+//
+// let otherString = 'some other';
+// let otherStringLength = otherString.length;
+// console.log(otherStringLength);
+
+
+// ------------GET STRING CHARACTER------------
+
+// let name = 'user';
+// let firstCharacter = name[0];
+// console.log(firstCharacter);
+
+
+// GET LAST CHARACTER;
+
+// let someString = 'ajsdhfsdgbfhknasjdnfhsgfbhasdbnfjsdfsdflkahdfjkashfjksdhfsdgfjahnsdbfgbdjsfhdbfhsdnfljsdAf';
+// let character = someString[someString.length - 1];
+// console.log(character)
+
+// ----------toLowerCase toUpperCase FUNCTIONS-----------
+
+// let username = 'Jhon Smith';
+// let lower = username.toLowerCase();
+// console.log(lower);
+// let upper = username.toUpperCase();
+// console.log(upper);
+
+// ---------indexOf method-----------
+// let name = 'jhon and hon';
+// let index = name.indexOf('o');
+// console.log(index);
+//
+// let noIndex = name.indexOf('f');
+// console.log(noIndex);
+
+//second argument after that
+// let newIndex = name.indexOf('o', 3) ;
+// console.log(newIndex);
