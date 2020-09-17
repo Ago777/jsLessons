e7f956416c6690748668243a32b7e4ca6e39752c
Grigoryan <argishtigrigoryan1991@gmail.com>
2020-09-18 03:25:17 +0400

    answers

--- lessons-month-1-week-2-day-1/homework/tasks.js
+++ lessons-month-1-week-2-day-1/homework/tasks.js
@@ -13,26 +13,12 @@
 
 //3) while cikli mijocov tpeq 10-100 mijakayqum bolor tver@ voronc tvanshanneri gumar@ mec e 12-ic
 
-// 4) while cikli mijocov tpeq 1-ic 10 mijakayqum gtnvox 3rd handipac zuyg tiv@;
-// let x = 1;
-// let y = 0;
-// while (x <= 10) {
-//     if(x % 2 === 0) {
-//         y++
-//     }
-//     if(y === 3) {
-//         console.log(x);
-//         break;
-//     }
-//     x++
-// }
-
-// 5) for loop-i mijocov reversedName popoxakanin veragreq name popoxakan@ ajic dzax grvac ev tpeq:
+// 4) for loop-i mijocov reversedName popoxakanin veragreq name popoxakan@ aynpes vor text@ lini ajic dzax grvac ev tpeq:
 //orinak ete name = 'JHON' petq e stanaq 'NHOJ';
 // let name = 'SNOITALUTARGNOC';
 // let reversedName = '';
 
-// 6) WHAT OUTPUT AND WHY
+// 5) WHAT OUTPUT AND WHY
 // let str = 'my name is ana';
 // let length = str.length - 1;
 // for (let i = 0; i < str.length; i++) {
@@ -42,15 +28,16 @@
 //    }
 // }
 
-// 7) WHAT IS OUTPUT
+// 6) WHAT IS OUTPUT
 // let x = 10 > 5;
 // while(x) {
 //     console.log('dont test it in console');
 //     x = !!x
 // }
 
-// 8) for loop-i mijocov tpeq 1-1000 mijakayqum bolor ayn tver@ voronq unen 1 kam 2 tvanshan orinak 5, 19, 57
+// 7) for loop-i mijocov tpeq 1-1000 mijakayqum bolor ayn tver@ voronq unen 1 kam 2 tvanshan orinak 5, 19, 57
 
+// 8) while cikli mijocov tpeq 1-ic 10 mijakayqum gtnvox 3rd handipac zuyg tiv@;
 
 // 9) for loop-i mijocov tpeq 1-1000 mijakayqum(1-@ ev 1000-@ neraryal) bolor kent tveri gumar@ ---->>  patasxan@ petq e stanaq 250000
 
