f7b6c9d6a0ef805e1e618fd9ccbd4760074c8ec8
argishti.grigoryan <argishti.grigoryan@betconstruct.com>
2020-09-18 19:00:20 +0400

    homework

--- lessons-month-1-week-1-day-3/homework/tasks.js
+++ lessons-month-1-week-1-day-3/homework/tasks.js
@@ -21,7 +21,26 @@
 //     default:
 //         console.log('i dont know')
 // }
+// RUZANNA ANSWER IS: switch(x);
+// NONA ANSWER IS switch(x);
+// ANAHIT ANSWER IS:
+// let x = 5;
+// switch (x)
+// {
+//     case x > 7:
+//     console.log('big');
+//     break;
 
+//     case x = 5:
+//     console.log('equal');
+//     break;
+
+//     default:
+//     console.log('i dont know');
+// }
+
+
+//---------------------------------------------------------
 
 //2) greq nuyn cod@ if else-i mijocov
 // let y = 'some';
@@ -46,6 +65,19 @@
 //     console.log('no text')
 // }
 
+//RUZANNA ANSWER IS:
+// if (y = 'some text'){
+//     console.log('some text') ;
+// } else if (y = 'some'){
+//     console.log('some');
+// }else {
+//     console.log('no text')
+// }
+
+
+//---------------------------------------------------------
+
+
 // 3) result popoxakanin veragreq anun@ ete ayn erkar e 4 ic hakarak depqum veragreq 'name is long' ev tpeq result@
 // let name = 'Jhon';
 // let result = null;
@@ -60,6 +92,12 @@
 // OR
 // result = name.length > 4 ? name : 'name is long';
 // console.log(result)
+// RUZANNA ANSWER CAN BE SHORT
+// NONA ANSWER CAN BE SHORT
+
+
+//---------------------------------------------------------
+
 
 // 4) haytarareq 3 popoxakan anun azganun tariq; ev greq script vorn tpum e 'done' ete bolor popoxakanner unen arjeq;
 
@@ -68,6 +106,23 @@
 // let surname = 'Doe';
 // let age = 30;
 // name && surname && age && console.log('done');
+
+// RUZANNA ANSWER IS:
+// 1. exanak
+// let done = 'Ruzanna'&&'Simonyan'&& 21;
+//     console.log('done')
+// //2.exanak
+// if (name && surname && 21){
+//     console.log('done')
+// }
+// RUDO ANSWER CAN BE SHORT
+// NONA ANSWER CAN BE SHORT
+// ANAHIT ANSWER CAN BE SHORT
+
+
+
+//-----------------------------------
+
 
 // 5) result popoxakanin veragreq anun@ ete ayn ka hakarak depqum azganun@;
 //       let username = 'Jhon';
@@ -79,6 +134,35 @@
 //       let surname = 'best';
 //       let result;
 // result = username || surname;
+//RUZANNA ANSWER IS:
+// if ( username){
+//     result = username;
+// }else {
+//     result = surname;
+// }
+
+// NONA ANSWER CAN BE SHORT
+
+//RUDO ANSWER IS:
+//DONT DO THIS CHECK
+// if (username != undefined) {
+//   result = username;
+// } else {
+//   result = surname;
+// }
+// console.log(result);
+// username != undefined ? (result = username) : (result = surname);
+// console.log(result);
+
+// wrong answer
+//result = surname ? surname : username;
+
+// only true anser
+//result = surname || username;
+
+
+
+//-----------------------------------------
 
 // 6) greq script vor kstugi ete user@ uni anun apa ayd anvann avelacni 777 ev tpi ayn;
 //    ete anun chuni ev tariq@ poqr e 18-ic tox stugi ete 16 kam 17 tarekan e apa tox tpi 'soon'
@@ -89,16 +173,49 @@
 // 6) ANSWER
 // let userName = 'Jhon';
 // let userAge = 17;
-// if (!!userName) {
+// if (userName) {
 //     userName += 777;
 //     console.log(userName)
 // }else if(userAge < 18) {
-//     if(userAge >= 16 || userAge <=17) {
-//         console.log('soon')
-//     }
+// userAge >= 16 || userAge <=17 ? console.log('soon') : null;
 // }else {
 //     console.log(userAge.toString())
 // }
+// RUZANNA ANSWER IS BEST
+// RUDO ANSWER IS:
+// let userName = "Jhon";
+// let userAge = 17;
+// if (userName) {
+//   console.log(userName + 777);
+// } else if (!userName && (userAge === 16 || userAge === 17)) {
+//   console.log("soon");
+// } else {
+//   console.log(userAge.toString());
+// }
+
+// ANAHIT ANSWER IS SAME RUDO ANSWER:
+//    if (userName)
+//    {
+//        console.log(userName + 777)
+//    }
+//    if (!userName && userAge < 18)
+//     {
+//        if (userAge >=16 || userAge <=17)
+//        console.log('soon')
+//     }
+//     else{
+//        console.log(userAge.toString());
+
+//     }
+
+
+
+//-----------------------------------------
+
+
+
+
+
 
 // 7) greq script vor@ kstugi ete popoxakan@ string e ev datark che apa kvercni stringi meji tiv@
 //    ev kstugi ete zuyg e ktpi;
@@ -106,7 +223,17 @@
 
 // 7) ANSWER
 // let numStr = '124String';
-// if (typeof numStr === 'string' && !!numStr) {
+// if (typeof numStr === 'string' && numStr) {
 //     let num = parseInt(numStr);
 //     num % 2 === 0 ? console.log(num) : null;
+// }
+
+
+// RUZANNA ANSWER IS:
+// let numStr = '124String';
+// if (typeof numStr === 'String' && numStr != '') {
+//     let n = parseInt(numStr);
+//     if (n % 2 === 0) {
+//         console.log(n)
+//     }
 // }
--- lessons-month-1-week-1-day-3/homework/what-output.js
+++ lessons-month-1-week-1-day-3/homework/what-output.js
@@ -9,6 +9,14 @@
 //     console.log(x)
 // }
 
+//  ANSWER IS 'true';
+
+// NONA ANSWER It will show "true"(WITHOUT QUOTES);
+// ANAHIT ANSWER IS  empty string is 0 . 0 = 0+1
+
+
+//----------------------------------------------------------------------
+
 // WHAT OUTPUT
 // 2)
 // let y = 10 <= '11' || null && 'string' > 5;
@@ -18,11 +26,60 @@
 //     console.log(!y)
 // }
 
+//  ANSWER IS false;
+
+
+
+//----------------------------------------------------------------------
+
+
+
+
+
 
 // WHAT OUTPUT
 // 3)
 // const x = (null || 'some text') && ('boolean' && 'null');
 // const res = x === 'null' ? console.log('result 1') : console.log('result 2');
+
+//  ANSWER IS 'result 1';
+
+// NONA ANSWER IS:
+// null returns false;
+// 'some text' is a not empty string which returns true;
+// true || true returns true;
+// In the next scope we have two not empty strings, and both of them returns true;
+// true && true returns true;
+// Finally we have true && true which returns true;
+// It means that our variable x is true;
+//const res = x === 'null' ? console.log('result 1') : console.log('result 2');
+// This is a shorthanded "if,else", which means that if the left part of the "?" is true, it returns 'result 1', else returns 'result 2';
+// At the left part of the "?" we have a variable res, which value is x;
+// Here x is equal to the value of 'null';
+// As null is written inside quotes, it's string, and string returns true;
+// As x was true, here we have true === true, which is true;
+// Program will show 'result 1'
+
+// RUZANNA ANSWER IS:
+// FIRST STRING X = TRUE;
+// res-i arjeqy havasar e x-in havasar e true kmtni if-i mej;
+// res-i arjeqy havasar e x-in havasar e true kmtni if-i mej;
+
+// ANAHIT ANSWER IS: NOT UNDERSTAND
+//result 1   && outputs the false value "null"
+
+//let x = ('boolean' || undefined) || ('' && null);
+//console.log(x);
+
+
+
+
+
+//----------------------------------------------------------------------
+
+
+
+
 
 
 // WHAT OUTPUT
@@ -33,14 +90,20 @@
 //     console.log(num && 5)
 // }
 
-// WHAT OUTPUT
-// 5)
-// let z = 10 <= '11' || null && 'string' > 5;
-// if (!z) {
-//     console.log(true || false)
-// } else {
-//     console.log(!z)
-// }
+//ANSWER IS 'no-num'; 5;
+
+//RUZANNA ANSWER IS:
+// num-i arjeqy false e kmtni else-i mej
+//true
+
+// ANAHIT ANSWER IS: NOT UNDERSTAND
+//num++ is 0 while num is 1 . since both 1,5 are true , && returns the last value
+
+
+//----------------------------------------------------------------------
+
+
+
 
 
 // WHAT OUTPUT
@@ -58,4 +121,9 @@
 //         }
 //     }
 // }
+//ANSWER IS 1
 
+//RUZANNA ANSWER IS
+//1
+
+// ANAHIT --> NO ANSWER
