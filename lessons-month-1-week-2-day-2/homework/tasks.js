00d4f4195866c21533ad0c35a96758cc6cc7bb32
Grigoryan <argishtigrigoryan1991@gmail.com>
2020-09-21 03:52:07 +0400

    answers

--- lessons-month-1-week-2-day-2/homework/tasks.js
+++ lessons-month-1-week-2-day-2/homework/tasks.js
@@ -61,15 +61,11 @@
 // age = 20;
 // getInfo(age);
 
-// greq function vor@ petq e stana  number argument ev stugi ete ayd tivn drakan e apa veradarcni minch ayd tiv@ exac bolor tveri gumar@;
+// greq function vor@ stanum e number argument ev drakan linelu depqum hashvum minchev ayd tiv@ exac tveri gumar@;
 //orinak ete functionn stana 3 apa petq e veradarcni 6 (1 + 2 + 3)
 
-// js@ uni function Math.max(num1, num2, num3) vor@ veradarcnum e trvac cankacac qanaki tveric amenamec@, greq ayd functionn inqnuruyn 3 parametri hamar
+// js@ uni function Math.max(num1, num2, num3) vor@ veradarcnum e trvac cankacac qanaki tveric amenamec@, greq ayd functionn inqnuruyn 3 tvi hamar
 
-//greq function vor naxatesvac e clone anelu hamar, aysinqn talis es erku argument 2rd@ datark ev function@ veradarcnum e 2rd argument@ arajini arjeqov
+//greq function vor@ stanum e 2 argument ev clone e anum 1@ 2rdi mej;
 
-// greq function vor@ stanum e 6 argument,
-// 2 tiv, 3 callback gumarum, hanum, ev bazmapatkum anelu hamar,
-//ev 1 string vori mijocov petq e stugel gumarel hanel te bazmapatkel tver@;
-
-// greq function vor@ kstana number argument ev khashvi te ayd numberi mej qani 0 ka;
+// greq function vor@ stanum e number argument ev khashvi te ayd numberi mej qani 0 ka;
