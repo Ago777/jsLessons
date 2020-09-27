102b20468b823940bee3a2fc2ff9aa41a8c3ec92
Grigoryan <argishtigrigoryan1991@gmail.com>
2020-09-27 23:49:02 +0400

    answers

--- lessons-month-1-week-3-day-2/homework/tasks.js
+++ lessons-month-1-week-3-day-2/homework/tasks.js
@@ -27,7 +27,7 @@
 //3. greq function vor@ stanum e 2 number type argument ev 1 string hetevyal operatorneric mekn@ --> '+', '-', '*', '/'
 // ev evradarcnum nshvac erku tveri tvyal operatori mijocov stacvac ardyunq@
 // orinak stanum e (5, '+', 7) kveradarcni 12
-
+//==========ANSWER==============
 // function makeMath(a, op, b) {
 //     switch (op) {
 //         case '+':
@@ -42,3 +42,37 @@
 // }
 //
 // console.log(makeMath(5, '-', 4));
+
+// 4. WHAT WILL BE THE OUTPUT
+
+// (function () {
+//     let a = b = 3;
+// })();
+// console.log(typeof a !== 'undefined');
+// console.log(typeof b !== 'undefined');
+
+//==========ANSWER==============
+// false, true
+
+// 5. WHAT WILL BE THE OUTPUT
+
+// (function () {
+//     var a = b = 3;
+// })();
+// console.log(typeof a !== 'undefined');
+// console.log(typeof b !== 'undefined');
+
+//==========ANSWER==============
+// false, true
+
+// // 6. WHAT WILL BE THE OUTPUT
+// function f1(f2) {
+//     return f2(5);
+// }
+// function f2(x) {
+//     console.log(x);
+// }
+// let f3 = f1(f2);
+// console.log(f3());
+//==========ANSWER==============
+// 5, error
