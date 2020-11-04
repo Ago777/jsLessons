4b2914c722597294a737003586c70a1abde6250d
Grigoryan <argishtigrigoryan1991@gmail.com>
2020-11-05 02:20:25 +0400

    answers

---
+++ lessons-month-1-week-4-day-1/closure.js
@@ -0,0 +1,133 @@
+// ==================CLOSURE=================
+// https://learn.javascript.ru/closure
+
+// let counter = 0;
+//
+// function makeCounter() {
+//     return counter++
+// }
+//
+// console.log(makeCounter());
+//
+// console.log(makeCounter());
+//
+// console.log(makeCounter());
+// this is a user click in magazine webstes
+// WE CAN CHANGE COUNTER FROM ANYWHERE
+
+//---------------------------------------------------------
+//WE CAN ADD COUNTER TO FUNCTION
+// function makeCounter() {
+//     let counter = 0;
+//     return counter++
+// }
+//
+// console.log(makeCounter());
+//
+// console.log(makeCounter());
+//
+// console.log(makeCounter());
+//---------------------------------------------------
+// function makeCounter() {
+//
+//     let count = 0;
+//
+//     return function () {
+//
+//         return count++
+//
+//     }
+// }
+//
+// const counter = makeCounter();
+//
+// console.log(counter());
+// console.log(counter());
+// console.log(counter());
+//
+// console.dir(makeCounter);
+// console.dir(counter);
+
+//-----------------------------------------
+// function makeCounter() {
+//
+//     let count = 0;
+//
+//     return function () {
+//
+//         return count++
+//
+//     }
+// }
+//
+// const counter1 = makeCounter();
+// const counter2 = makeCounter();
+//
+// console.log(counter1());
+// console.log(counter1());
+// console.log(counter1());
+//
+// console.log(counter2());
+// console.log(counter2());
+// console.log(counter2());
+// INSTEAD THIS WE MUST WRITE LIKE THIS
+// let counter1 = 0;
+// let counter2 = 0;
+//
+// function makeCounter1() {
+//     return counter1++
+// }
+// function makeCounter2() {
+//     return counter2++
+// }
+
+// ----------make function for double numbers then for 10------
+
+// function getDouble(num) {
+//     return num * 2
+// }
+//
+// function getTenFold(num) {
+//     return num * 10
+// }
+
+// function getValue(num1) {
+//     return function (num2) {
+//         return num1 * num2
+//     }
+// }
+//
+// const getDouble = getValue(2);
+// getDouble(4);
+//
+// const getTenFold = getValue(10);
+// getTenFold(5);
+
+// getValue(2)(4);
+// getValue(10)(4);
+
+//THIS IS CLOSURE
+
+
+//-------------GARBAGE COLLECTOR----------------
+//Typically, the lexical environment is cleaned up and removed after the function has completed. For instance:
+// But, if there is a nested function that is still available after executing f,
+// then it has a [[Environment]] property that refers to the external lexical environment;
+
+// function f() {
+//     let x = 5;
+//     let y = 4;
+// }
+//
+// f();
+//
+// function f() {
+//     let x = 5;
+//     let y = 4;
+//     return function () {
+//         console.log(x)
+//     }
+// }
+//
+// f();
+
---
+++ lessons-month-1-week-4-day-1/homework/tasks.js
@@ -0,0 +1,85 @@
+//1) CLOSURI MIJOCOV STACEQ GUMARMAN FUNCTION;
+// aysinqin f(1)(2) ---> ksatananq 3
+
+
+// 2) CLOSURI MIJOCOV STEXCEQ FUNCTION VOR@  ERKROD KANCHI ZHAMANAK STACVAC TIV@ KLCNI ARRAY MEJ,
+// ARAJIN KANCHI ZHAMANAK STACVAC TVI CHAPOV;
+
+// orinak  f(5)(4) --> knshanaki stanal array vori mej 5 klini 5 hat 4 tiv@ ----> [4,4,4,4,4];
+//======ANSWER======
+
+// 3) INCH TARBERUTYUN TVYAL ARTAHAYUTYUNNERI MEJ
+
+// let arr = [];
+// var arr = [];
+// const arr = [];
+
+// 4) HAYTARAREQ 3 TESAKI FUNCTION (DECALARTION, EXPRESSION, ARROW FUNCTION) EV NSHEQ TARBERUTYUNNERN
+
+// 5) INCH KTPI XI?
+// for (let i = 0; i < 5; i++) {
+//     console.log(i++);
+// }
+
+//6) INCH KTPI XI?
+// let a = 2
+// while(a !== 5) {
+//     if (a > 0) {
+//         a+=1;
+//         console.log(a++)
+//     } else {
+//         console.log(++a)
+//     }
+//     a += 1;
+// }
+
+// 7 INCH KTPI XI?
+// var b = 3;
+// let a =5;
+// do {
+//     if (++a % 3 === 0 && b % 2 === 0) {
+//         console.log(1);
+//     } else if (a === 5 || a % 2 === 0) {
+//         console.log(2);
+//     }
+// } while (a < 8);
+
+//8) INCH KTPI U XI?
+// function foo () {
+//     switch(true) {
+//         case a > 2:
+//             console.log(3);
+//             break;
+//         case a <= 1:
+//             console.log(2);
+//             break;
+//         case a <= 1:
+//             console.log(4);
+//             break;
+//         default:
+//             console.log(1);
+//             break;
+//     }
+// }
+// let a = 1;
+// foo();
+
+//9 INCH KTPI U XI ?
+// function sayWelcome (name) {
+//     let message = 'hello ' + name;
+//     return function() {
+//         console.log(message);
+//     };
+// }
+// sayWelcome("Henry")();
+
+// 10 INCHPES KANCHEL FUNCTION
+// function logger() {
+//     return function () {
+//         return function () {
+//             console.log("Message");
+//         }
+//     }
+// }
+
+
