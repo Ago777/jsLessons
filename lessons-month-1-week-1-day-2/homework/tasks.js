360c4b35e1943abb92a749ebd380adf82f6e7715
argishti.grigoryan <argishti.grigoryan@betconstruct.com>
2020-12-11 14:56:31 +0400

    homework

--- lessons-month-1-week-1-day-2/homework/tasks.js
+++ lessons-month-1-week-1-day-2/homework/tasks.js
@@ -5,6 +5,43 @@
 //  bazmapatkeq js-i koxmic generacvox tvov
 //  tpeq stacvac arjeq@ kotorakayin tesqov aynpes vor ketic heto lini 2 tiv orinak 2.22
 
+// --ANSWER IS:---
+// let num = 132;
+// let num2 = 456;
+// let sum = num.toString() + num2.toString();
+// let preLastElem = sum[sum.length - 2];
+// console.log(preLastElem);
+// let result = preLastElem * Math.random();
+// console.log(result.toFixed(2));
+
+//-----ELINA ANSWER IS---
+//let x=255;
+// let y=465;
+//
+// let stringX=x.toString();
+// let stringY=y.toString();
+//
+// let sum= a.toString() +b.toString();
+// console.log(sum);
+//
+// let character = sum[sum.length - 2];
+// console.log(character)
+//
+// let all=255465*6;
+// console.log(all);
+//
+// let fixedNum = all.toFixed(2);
+// console.log(fixedNum);
+
+//--- RUZANNA ANSWER IS----
+// let a =713;
+// let b = 539;
+//
+// let stringA = a.toString();
+// let stringB = b.toString();
+// let stringSam = stringA + stringB;
+// console.log(((stringSam.length - 2) * Math.random()).toFixed(2));
+
 
 // 2)
 // haytarareq popoxakan ev veragreq 0: 6 angam tpeq popoxakan@ aynpes vor tpi 0, 1 ev 2 tvern hetevyal hertakanutyamb
@@ -15,11 +52,55 @@
 // 2
 // 0
 
+// ---ANSWER IS---
+// let x = 0;
+// console.log(x++);
+// console.log(++x);
+// console.log(--x);
+// console.log(x++);
+// console.log(x--);
+// console.log(--x);
+
+//---ELINA ANSWER IS---
+// true answer
+// let x=0;
+// console.log(x);
+// x++;
+// x++;
+// console.log(x);
+// x--;
+// console.log(x);
+// console.log(x);
+// x++;
+// console.log(x);
+// x--;
+// x--;
+// console.log(x);
+
+
 // 3)
 // haytarareq 2 popoxakan ev veragreq '123javascript';
 // tvyal popoxakanic staceq meji tiv@
 // aynuhetev tpeq tvyal tvi tvanshanneri gumarn
 
+// ---ANSWER IS---
+// let x = '123javascript';
+// let num = parseInt(x);
+// let stringNum = num.toString();
+// let result = +stringNum[0] + +stringNum[1] + +stringNum[2];
+// console.log(result);
+
+// ---ELINA ANSWER IS--
+// let x="123javascript";
+// let y="123javascript";
+// let parsedXInt=parseInt(x);
+// console.log(parsedXInt);
+//
+// let parsedYInt=parseInt(x);
+// console.log(parsedYInt);
+//
+// sum= parsedXInt+parsedYInt;
+// console.log(sum);
 
 // 4) Aranc 3rd popoxakan haytararelu poxeq arjeqneri texer@: aynpes vor  num1- @ darna 67890, num2 @ darna 12345;
 //chi kareli ogtagorcel vochmi ayl tiv ev ayl popoxakan
@@ -27,4 +108,23 @@
 // let num1 = 12345;
 // let num2 = 67890;
 
+// ---ANSWER IS---
 
+// let num1 = 12345;
+// let num2 = 67890;
+// num1 = num1 + num2;
+// num2 = num1 - num2;
+// num1 = num1  - num2;
+// console.log(num1, num2)
+
+// ELINA ANSWER IS:
+// no answer
+
+// NONA ANSWER IS:
+// num2=num1 +(num1=num2)-num2;
+// console.log(num1,num2);
+
+// RUZANNA ANSWER IS:
+// num1 = num2;
+// num2 = num1;
+// console.log(num1,num2);
--- lessons-month-1-week-1-day-2/homework/what-output.js
+++ lessons-month-1-week-1-day-2/homework/what-output.js
@@ -5,7 +5,11 @@
 //     let d = c.toString();
 //     d += d;
 //     console.log(d);
-//
+
+// ---ANSWER----
+// 'truetrue'
+
+
 // // 2) WHAT WILL BE THE OUTPUT AND WHY
 //
 //     let x;
@@ -13,6 +17,10 @@
 //     let z = Number(y);
 //     let q = z++;
 //     console.log(q);
+
+// ---ANSWER----
+// 0
+
 //
 // // 3) WHAT WILL BE THE OUTPUT AND WHY
 //
@@ -22,6 +30,11 @@
 //     let sum = 10 + str4;
 //     console.log(sum);
 //
+// ---ANSWER----
+// '10 '
+
+// RUZANNA && ELINA ANSWER IS '10';
+
 // // 4) WHAT WILL BE THE OUTPUT AND WHY
 //
 //     let num = 124.5;
@@ -32,6 +45,12 @@
 //     const result = something >= '';
 //     console.log(result);
 //
+// ---ANSWER----
+//true
+
+// ELINA ANSWER IS:
+// 125 is more or equal than UNDEFINED, so the answer is true
+
 // // 5) WHAT WILL BE THE OUTPUT AND WHY
 //
 //     let bool = true;
@@ -42,7 +61,10 @@
 //     let newLen = len + +'5.12345';
 //     const res = newLen.toFixed(1);
 //     console.log(res);
-//
+
+// ---ANSWER----
+// '12.1'
+
 // // 6) WHAT WILL BE THE OUTPUT AND WHY
 //
 //     let lastString = 'string';
@@ -51,6 +73,13 @@
 //     let isEqual = getSymbol === 'g';
 //     console.log(isEqual);
 //
+// ---ANSWER----
+// true
+
+// ELINA ANSWER IS:
+//whatOutput is 1, (s-0, t-1,r-2...)
+//lastString.length is 6
+
 // // 7) WHAT WILL BE THE OUTPUT AND WHY
 //
 //     let age = 44;
@@ -60,12 +89,31 @@
 //     let isEqual2 = newAge === newStringAge;
 //     console.log(isEqual2);
 //
+// ---ANSWER----
+//false
+
+// ELINA ANSWER IS:
+//newAge is 44
+//as newAge is postFixValue, in the first line it's not seen, but in stringAge it becomes "45"
+//newStringAge becomes "45"
+//newAge is 45, and newStringAge is "45", so they are not equal, the ANSWER is false
+// RUZANNA ANSWER IS:
+//     let age = 44;
+//     let newAge = age++;             //44 (gumarvum e 1 bayc darnum e 45 hajord toxum)
+//     let stringAge = age.toString(); //'45'
+//     let newStringAge = +stringAge;  // 45
+//     let isEqual2 = newAge === newStringAge; //45===45
+//     console.log(isEqual2);                  //true
+
 // // 8) WHAT WILL BE THE OUTPUT AND WHY
 //
 //     let randomNumber = Math.random();
 //     let resultNum = Math.round(randomNumber);
 //     console.log(resultNum);
 //
+// ---ANSWER----
+// 0 or 1
+
 // // 9) WHAT WILL BE THE OUTPUT AND WHY
 //
 //     let last = 'last question';
@@ -74,3 +122,9 @@
 //
 //     console.log(lastResultString.toUpperCase());
 // //don't write code like this never
+
+// ---ANSWER----
+// '9 IS A LAST QUESTION'
+
+//ELINA NO ANSWER
+
