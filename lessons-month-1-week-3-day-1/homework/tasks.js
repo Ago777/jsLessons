6d3ae2f3103aa7f99021679d17e6e6e32ea3ee15
Grigoryan <argishtigrigoryan1991@gmail.com>
2020-12-21 23:08:50 +0400

    jghj

--- lessons-month-1-week-3-day-1/homework/tasks.js
+++ lessons-month-1-week-3-day-1/homework/tasks.js
@@ -1,132 +1,141 @@
-// bolor patasxannern petq e linen universal aysinqn cankacac argument poxancelu depqum petq e chisht ashxati kam
-// chashxatelu depqum console.log aneq inchvor text nshelov vor sxal argument eq stacel;
-// error chpiti lini
-// orinak  ete petq e greq function vor kstana inchvor tiv kam array,
-// da chi nshanakum vor ete es sxalvem urish type poxancem petqa kod@ traqi ayl petqa inchvor textov zgushacni vor sxal argumentem poxancel;
-
-// 1) greq function vor@ kstana array ev khashvi ayd arrayum exac tveric amenamec@ (arrayum karan tver chlinen);
+// 1) greq function vor@ vorpes argument kstana array ev ktpi tvyal arrayum exac bolor tver@
 // ======= ANSWER ==========
-//     function getMaxFromArr(arr) {
-//         if(!Array.isArray(arr)) return;
-//         let filteredArr = arr.filter(item => typeof item === 'number');
-//         if(!filteredArr.length) return 'no numbers in array';
+// function logNumbers(arr) {
+//     // for(let i = 0; i< arr.length; i++) {
+//     //     if(typeof arr[i] === 'number') {
+//     //         console.log(arr[i])
+//     //     }
+//     // }
+// }
 //
-//         return Math.max(...filteredArr)
-//     }
-//     console.log(getMaxFromArr([5,7,9]))
+// logNumbers([7,'someText', null, 87])
+// ======= YOUR ANSWERS =======
 
 
-// 2) greq function vor@ kstana array ev mek primitive type;
-//    ev kstugi te tvyal primitive type-ic qani hat ka arrayum u kveradarnci ayd qanak@
-
-// ======= ANSWER ==========
-//     function isIncludedValue(arr, value) {
-//         if(!Array.isArray(arr)) return;
-//         let result = arr.filter(item => item === value);
-//
-//         return result.length;
-//     }
-//
-//     console.log(isIncludedValue([5,7,'some', 7], 7))
-
-// 3) greq function vor@ kstana 2 array ev kstugi ardyoq irenc parunakutymab lriv nuynn en,
-// ete ayo kveradarcni true hakarak depqum ayd erku arraynern kmiacni kdarcni mek array ev kveradarcni:
-// orinak ---> [1,2,3] ev [1,2,3] kveradarcni true -->['a','b'] ev ['a', 'b', 'c'] --> kdarcni ['a', 'b', 'a', 'b', 'c']
-
+// 2) greq function vor@ kstana 2 array argument ev ktpi aveli erkar arrayi meji bolor string elementner
 // ======= ANSWER ==========
 
-// function compare(arr1, arr2) {
-//     if (!Array.isArray(arr1) || !Array.isArray(arr2)) return;
-//     if (arr1.length !== arr2.length) return [...arr1, ...arr2];
-//     for (let i = 0; i < arr1.length; i++) {
-//         if(arr1[i] !== arr2[i]) return false
-//     }
-//     // let comparedArr = arr1.map((item,i) => item === arr2[i]);
+// function logStrings(arr1, arr2) {
+//     // let arr = arr1.length > arr2.length ? arr1 : arr2;
+//     // for(let i = 0; i< arr.length; i++) {
+//     //     if(typeof arr[i] === 'string') {
+//     //         console.log(arr[i])
+//     //     }
+//     // }
+//     // arr.forEach(item => typeof item === 'string' ? console.log(item) : null)
+// }
+//
+// logStrings([7,'someText', null, 87], [5,9])
+
+// ======= YOUR ANSWERS =======
+
+
+// 3) greq function vor@ kstana erku array voric mek@ datark e, ev myus arrayi meji elementern klcni datark arrayi mej u kveradarcni ayd array@
+// ======= ANSWER ==========
+// function cloneArray(arr, emptyArr) {
+//     // for(let i = 0; i< arr.length; i++) {
+//     //     emptyArr.push(arr[i])
+//     // }
 //     //
-//     // return comparedArr.includes(false) ? [...arr1, ...arr2] : true;
+//     // return emptyArr
+//     // arr.forEach(item =>  emptyArr.push(item))
+//     // return emptyArr
 // }
-
-// console.log(compare([1, 9, 'string'], [1, 9, 'string']));
-
-// 4) greq function vor@ kstana array, ayd arrayi mejic karandznacni bolor duplicate elementnern,
-// (aysinqn nuyn arjeqnern orinak erku hat 5 ete lini) ev kveradarcni nor array aranc duplicate-i
-// orinak---> ete poxnacem [5,7, 'some', 5, true, 'some', false, true] petq e functionn return ani [5,7, 'some', false, true]
-
-// ======= ANSWER ==========
-// function makeArrUnique(arr) {
-//     if(!Array.isArray(arr)) return;
 //
-//     let result = [];
-//     arr.forEach(item => {
-//         if (!result.includes(item)) {
-//             result.push(item)
-//         }
-//     });
-//     return result;
-// }
-// OR
-// function makeArrUnique(arr) {
-//     if (!Array.isArray(arr)) return;
-//     return arr.filter((item, i) => arr.indexOf(item) === i);
-// }
+// console.log(cloneArray([7, 'someText', null, 87], []));
 
 // ======= YOUR ANSWERS =======
 
-// 5) greq function vor kstana array vori elementneric gone mekn eli array e ev erku callback;
-//       function@ petq e hashvi bolor tveri gumar@ naev ayn tveri voronq gtnvum en nested arrayi mej ev nested arrayneri qanak@;
-//    arajin calbback petq e tpi patasxan@ ev nshi te qani array type- i element e exel himnakan arrayi mej;
-//     erkrord callback petq e tpi 0 ete vochmi number EV vochmi array type-i element chi exel himanakn arrayum
-//   orinak tvual arrayi depqum--> [5, [4, 'a'], true, ['c', 1, 'b']];
-//  petq e ashxati arajin callback@ ev tpi 10(bolor tveri gumar@) heto 2(array elementeri qanak@)
+// 4) greq function vor@ stanum 2 argument mek@ array myus@ number type-i; ev stugum e ete arrayum ka 2rd argumentov stacvac tiv@
+// apa veradarcnum e ayd tiv@  hakarak depqum veradarcnum e false
 
 // ======= ANSWER ==========
-// function successCb(sum, count) {
-//     console.log(sum);
-//     console.log(count)
+// function showNum(arr, num) {
+//     // for (let i = 0; i < arr.length; i++) {
+//     //     if (arr[i] === num) {
+//     //         return num
+//     //     }
+//     //
+//     // }
+//     // return false
+//
+//     // return arr.includes(num) ? num : false
 // }
 //
-// function errorCb() {
-//     console.log(0);
+// console.log(showNum([7, 'someText', null, 87], 5));
+
+// ======= YOUR ANSWERS =======
+
+
+// 5) greq function vor kstana 4 argument bolor@ number ev kveradarcni array vori mej klinen ayd 4 numberneri krknapatiknern
+// ======= ANSWER ==========
+// function convertToArr(a, b, c, d) {
+//     // return [a*2, b*2, c*2, d*2];
+//     //
+//     // let arr = [];
+//     // arr.push(a * 2);
+//     // arr.push(b * 2);
+//     // arr.push(c * 2);
+//     // arr.push(d * 2);
+//     // return  arr
 // }
 //
-// function doSomething(arr, successCb, errorCb) {
-//     if (!Array.isArray(arr)) return;
+// console.log(convertToArr(4, 8, 7, 9));
+
+
+// ======= YOUR ANSWERS =======
+
+
+// 6) greq function vor@ stanum e number argument ev drakan linelu depqum hashvum minchev ayd tiv@ exac tveri gumar@;
+//    orinak ete functionn stana 3 apa petq e veradarcni 6 (1 + 2 + 3)
+
+// =======ANSWER =======
+
+// function showSum(num) {
 //     let sum = 0;
-//     let count = 0;
-//     arr.forEach(item => {
-//         if (typeof item === 'number') {
-//             sum += item
-//         } else if (Array.isArray(item)) {
-//             count++
-//             item.forEach(nestedItem => {
-//                 if (typeof nestedItem === 'number') {
-//                     sum += nestedItem;
-//                 }
-//             })
-//         }
-//     });
-//     if (!sum && !count) {
-//         typeof errorCb === 'function' && errorCb()
-//     } else {
-//         typeof successCb === 'function' && successCb(sum, count)
+//     for (let i = 1; i < num; i++) {
+//         sum += i
 //     }
+//     return sum
 // }
 //
-// console.log(doSomething([5, [4, 'a'], true, ['c', 1, 'b']], successCb, errorCb))
-
+// console.log(showSum(5));
 // ======= YOUR ANSWERS =======
 
 
-//6) greq function vor@ stanum e array miayn number elementnerov ev veradarcnum
-//nor array bolor ayn elementneri krknapatikov voronq zuyg en (poqric mec dasavorutyamb)
-// oribnak--> stanum enq [5,2,4,1,9,6] ---> veradarcnum e [4,8,12] aysinqn 2,4,6 -i krknapatiknern poqric mec dasavorutyamb
-// function getNewArray(arr) {
-//     if (!Array.isArray(arr)) return 'argument ios not array';
-//     const filteredArr = arr.filter(item => item % 2 === 0);
-//     const mappedArr = filteredArr.map(item => item * 2);
-//     return mappedArr.sort((a, b) => a - b)
-// }
 
+// 7) greq function vor@ stanum e number argument ev khashvi te ayd numberi mej qani 0 ka;
+// =======ANSWER =======
+// function showZerosCount(num) {
+//     // let stringNum = num.toString();
+//     // let count = 0;
+//     // let i = 0;
+//     // while(i < stringNum.length) {
+//     //     if(+stringNum[i] === 0) {
+//     //         count++
+//     //     }
+//     //     i++
+//     // }
+//     //
+//     // return count
+//
+//     // let count = 0;
+//     // let i = 0;
+//     // while(i < num) {
+//     //     let lastDigit = num % 10;
+//     //     if(lastDigit === 0) {
+//     //         count ++
+//     //     }
+//     //     num = (num - lastDigit) / 10;
+//     //     i++
+//     //
+//     // }
+//     // return count
+// }
+//
+// console.log(showZerosCount(1205));
+
+// ======= YOUR ANSWERS =======
 // ERRORNERI DEPQUM HSTAK ASEQ INCH ERROR KBERI U XI
 // 7)WHAT WILL BE THE OUTPUT AND WHY
 // var x = 12;
