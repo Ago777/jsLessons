66228552433ac86a3caf60fb78769ad332d4f9fc
argishti.grigoryan <argishti.grigoryan@betconstruct.com>
2020-12-21 13:26:51 +0400

    homework

---
+++ lessons-month-1-week-3-day-1/homework/links.js
@@ -0,0 +1,8 @@
+//========HOISTING=========
+// https://medium.com/javascript-in-plain-english/https-medium-com-javascript-in-plain-english-what-is-hoisting-in-javascript-a63c1b2267a1
+
+// ============SCOPE===================
+// https://dmitripavlutin.com/javascript-scope/#:~:text=In%20JavaScript%2C%20scopes%20are%20created,variables%20of%20an%20outer%20scope.
+
+// =======VAR===
+// https://learn.javascript.ru/var
