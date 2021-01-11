07272db792d3f8821c862d16698e780b4bcc9b81
argishti.grigoryan <argishti.grigoryan@betconstruct.com>
2021-01-11 16:56:05 +0400

    homework

--- holiday-homework/tasks.js
+++ holiday-homework/tasks.js
@@ -27,6 +27,22 @@
 // checkNum(0);
 
 // //====================YOUR ANSWERS=====================
+// Nona answer is: typeof number != "number"
+// Elina and Vahag naswer is:
+// function checkNum(a) {
+//     switch (Math.sign(a)) {
+//         case 1:
+//             console.log("it is plus");
+//             break;
+//         case -1:
+//             console.log("it is minus");
+//             break;
+//         case 0:
+//             console.log("it is 0");
+//             break;
+//     }
+// }
+// checkNum(-25);
 
 // 2) greq function vorn stanum cankacac tiv ev stugum e ete tvanshannern 3-ic shat en hashvum e gumar@ ayd tvanshanneri ete qich en;
 //     hashvum e artadryal@; ete henc eranish tiv e tox hamapatasxan textov asi vor eranish e;
@@ -58,6 +74,21 @@
 // console.log(workWithDigits(125));
 
 //====================YOUR ANSWERS=====================
+// Elina answer is:
+//  function getNum (arr){
+//    if (!Array.isArray(arr)) return "no array";
+//      let check= arr.map(function(item){
+//          if (arr.length >3){
+//             return item+item;
+//          } else (arr.length<3)   {
+//              return item*item);
+//          }else (arr.length=3){
+//             return "This number has 3 digits";
+//          }
+//      }
+//  }
+//  getNum ([123])
+// Vahag no answer:
 
 // 4) greq function vor stanum e tverov array ev sortavourm e ayd array@ ka hamapatasxan method;
 // orinak stanum e [5,0,7,1] veradarcnum [0,1,5,7]
@@ -75,6 +106,14 @@
 
 //====================YOUR ANSWERS=====================
 
+// Vahag answer is:
+// function toGrow(numbers) {
+//     let compareNumbers = (a, b) => a - b;
+//     console.log(numbers.sort(compareNumbers));
+// }
+// toGrow([5, 8, 7, 9, 6, 1, 2, 3, 20, 30, 45, 21, 125]);
+// Ամենաօպտիմալ տարբերակն էս էր, բայց մի բան մենակ լավ չեմ հասկացել, a-b պահը function-ի return-ի մեջ։
+
 // 5) greq function vor stanum e array ev veradarcnum e mek ayl array vortex klinen naxord arrayi miayn tver@;
 // orinak stanum e [5,0, 'some', null] veradarcnum [5,0]
 
@@ -91,6 +130,43 @@
 // console.log(toFilter(['str', 0, '2', true, 7]));
 
 //====================YOUR ANSWERS=====================
+// ALL ANSWERS IS TRUE
+//Nona answer is:
+// function getNumbers(arr){
+//     if (!Array.isArray(arr)) return 'This is not array';
+//     let newArr = [];
+//     for(let i = 0 ; i < arr.length ; i++){
+//         if (typeof arr[i] === 'number'){
+//             newArr.push(arr[i]);
+//         }
+//     }
+//     return newArr;
+// }
+// console.log(getNumbers([12,5,'Nona']));
+
+// Elina answer is:
+// function getNumbers(arr){
+// if (!Array.isArray(arr)) return "no array";
+//     let arr1=[];
+//     for(let i=0; i<arr.length; i++){
+//         typeof arr[i]==="number"? arr1.push(arr[i]) : null;
+//     }
+//     return arr1;
+// }
+//
+// console.log(getNumbers([true, false, "string", 1, 2, "hello", 5, 85]));
+
+// Vahag answer is:
+//     function toSeparate(firstOne) {
+//         let secondOne = [];
+//         for (let i = 0; i < firstOne.length; i++) {
+//             typeof firstOne[i] === "number" ? secondOne.push(firstOne[i]) : null;
+//         }
+//         console.log(secondOne);
+//     }
+//
+// toSeparate(["some text", "5", 8, 9, 565, "The end"]);
+// If-ի կարճ գրելաձևի մեջ առանց else չի՞ լինում։
 
 // 6) greq function vor stanum e array ev veradarcnum e mek ayl array vortex klinen naxord arrayi stringneri arajin elementnern;
 // // orinak stanum e [5,0, 'some', 'javascript'] veradarcnum ['s', 'j']
@@ -128,6 +204,36 @@
 // console.log(getFirstSymbols([5, 0, 'some', 'javascript']));
 
 // //====================YOUR ANSWERS=====================
+// ALL ANSWERS IS TRUE
+//Nona and Vahag answers answer is:
+
+// function getFirstElementOfString(arr){
+//     if (!Array.isArray(arr)) return 'This is not array';
+//     let stringArr = [];
+//     let finalArr = [];
+//     for (let i = 0 ; i < arr.length ; i++){
+//         if( typeof arr[i] === 'string'){
+//             stringArr.push(arr[i]);
+//         }
+//     }
+//     for (let i = 0 ; i < stringArr.length ; i++){
+//         finalArr.push(stringArr[i][0])
+//     }
+//     return finalArr;
+// }
+// console.log(getFirstElementOfString(['Nona', 13 , 15 , 'Karapetyan']));
+
+//Elina answer is:
+// function getNumbers(arr){
+//   if (!Array.isArray(arr)) return "no array";
+//     let arr1=[];
+//     for(let i=0; i<arr.length; i++){
+//         typeof arr[i]==="string"? arr1.push(arr[i][0]) : null;
+//     }
+//    console.log (arr1);
+// }
+// getNumbers([true, false, "string", 1, 2,"hello", 5, 85,"name", "surname"]);
+
 
 // 7) greq function vor stanum e array ev veradarcnum e ayd arrayi tvanshanneri gumar@ (methodov ashxateq greq vochte for loopov)
 
@@ -163,7 +269,27 @@
 // console.log(sumOfNumbers([4, 'str', 124, '22']));
 
 //====================YOUR ANSWERS=====================
+// Nona and Elina answer is:
 
+// function getSumOfElements(arr){
+//     if (!Array.isArray(arr)) return 'This is not array';
+//     let onlyNums = [];
+//     for (let i = 0 ; i < arr.length ; i++){
+//         if (typeof arr[i] === 'number'){
+//             onlyNums.push(arr[i]);
+//         }
+//     }
+//     return onlyNums.reduce((sum, item) => sum + item);
+// }
+// console.log(getSumOfElements([12, 'n' , 5 ,4 , 'yes']));
+
+// Vahag answer is:
+// function toPlus(arr) {
+//     let result = arr.reduce((sum, current) => sum + current);
+//     console.log(result);
+// }
+//
+// toPlus([5, 8, 9, 10, 15]);
 
 // 8) greq function vor@ stanum e tveri array ev mek number argument ev veradarcnum nor array vori mej klinen miayn number argumentov stacvac tvic mec tver@
 //       orinak stanum e [4,1,5], 3 petq e verdarcni kveradarcni [4,5]
@@ -172,7 +298,7 @@
 
 // function onlyHighNumbers(arr, num) {
 //     if(Array.isArray(arr)) {
-//         let res = arr.filter(item => item > num);
+//         let res = arr.filter(item => item > num && typeof item === 'number');
 //         return res
 //     }
 //
@@ -182,6 +308,19 @@
 // console.log(onlyHighNumbers([1, 5, 7, 0, 15], 5));
 
 //====================YOUR ANSWERS=====================
+//Nona and Elina and Vahag asnwer is:
+//  function getBiggerNums(arr , number){
+//      if (!Array.isArray(arr)) return 'This is not array';
+//      let newArr = [];
+//      for (let i = 0 ; i < arr.length ; i++){
+//          if (arr[i] > number) {
+//              newArr.push(arr[i]);
+//          }
+//      }
+//      return newArr;
+//  }
+//  console.log(getBiggerNums([12 , 5 , 16], 7));
+//  console.log(getBiggerNums([12 , 5 , 16, '45', true], 0));
 
 // 9) greq function vor@ kstana 3 argument arajin erkusn number, errord@ hetevyal stringneric mekn 'sum' 'divide' 'minus' 'multiply')
 //    ev kaxvac errod argumentic kkatari ayd tveri het hamapatasxan gorcixutyun ev kveradarcni arjeq@;
@@ -216,6 +355,31 @@
 // console.log(doMath(10, 5, 'asf'));
 
 //====================YOUR ANSWERS=====================
+// Nona answer is:
+// function calculate(num1 , num2 , option){
+//     switch (option){
+//         case 'sum':{
+//             return num1+num2;
+//             break;
+//         }
+//         case 'divide':{
+//             return num1/num2;
+//             break;
+//         }
+//         case 'minus':{
+//             return num1 - num2;
+//             break;
+//         }
+//         case 'multiply':{
+//             return num1*num2;
+//             break;
+//         }
+//         default:{
+//             return "JS can't calculate the option you have entered.";
+//         }
+//     }
+// }
+// console.log(calculate(2 , 5 , 'sum'));
 
 // 10) greq function vor@ stanum e array argument filtrum e u stanum nuyn array@ bayc menak tverov
 //    ev stugum ete ayd arrayi arajin u verjin tveri gumar@ mec e 10 ic apa kveradarcni true hakarak depqum false
@@ -240,6 +404,18 @@
 
 
 //====================YOUR ANSWERS=====================
+// Elina no asnwer
+// Vahag answer is:
+// function diffArguments(mixedArr) {
+//     let onlyNumbers = mixedArr.filter(item => typeof item === "number");
+//     if (onlyNumbers[0] + (onlyNumbers.length - 1) > 10) {
+//         console.log(true);
+//     } else {
+//         console.log(false);
+//     }
+// }
+//
+// diffArguments(['start', 5, 8, 7, 9, 10, 'some text', 58, 85, 'the end'])
 
 // 11) greq function vor kstana array ev ktpi ayd arrayi amen KENT INDEX unecox elementern
 //    ogtagorceq foreach: orinak kstana [5,7,'some', 'js']  ktpi 7, 'js'
@@ -259,7 +435,41 @@
 // showOddIndexes([12, '54', true, 'str', 55]);
 
 //====================YOUR ANSWERS=====================
+// Nona answer is:
+// function getOddIndexElement(arr){
+//     if (!Array.isArray(arr)) return 'This is not array';
+//     arr.forEach(item => {
+//         if(arr.indexOf(item) % 2 !== 0){
+//             console.log(item);
+//         }
+//     })
+// }
+// getOddIndexElement([12 , 45 ,65 , 100 , 'yes' , 'no']);
 
+// Elina answer is:
+// function getNum(arr) {
+//     if (!Array.isArray(arr)) return "no array";
+//     let odd = arr.forEach(function (item, index) {
+//         if (item[index] % 2 !== 0) {
+//             console.log(item);
+//         }
+//     });
+// }
+//
+// console.log(getNum([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+
+// Vahag answer is:
+// function oddOrEven(arr) {
+//     let result = [];
+//     arr.forEach((x, i) => {
+//         if (i % 2 === 1) {
+//             result.push(x);
+//         }
+//     });
+//     console.log(result);
+// }
+//
+// oddOrEven([5, 'some text', 8, 'JS', 'The End']);
 
 // 13) greq function vor kstana array ev mek cankacac arjeq ev kstugi ete ayd 2rd argumentov stacvac arjeqn ka arrayum
 //   uremn kveradarcni ayd ayd arjeq@ hakarak depqum kveradarcni hamapatasxan text vor chka
@@ -279,6 +489,37 @@
 // console.log(findItem([12, 'str', 'name', 44], 'surname'));
 
 //====================YOUR ANSWERS=====================
+// Nona and Elina answer is:
+
+// function findSameMember(arr, value) {
+//     if (!Array.isArray(arr)) return 'This is not array';
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] === value) {
+//             return value;
+//         }
+//     }
+//     return 'No this member';
+// }
+// console.log(findSameMember([3,5,7,'some word'], 7));
+
+// Vahag answer is
+
+// Example 1
+// function toFindArr(arr, text) {
+//     arr.forEach((item) => {
+//         if (item === text) {
+//             console.log(text)
+//         }
+//     });
+// };
+// toFindArr(['who knows', 8, 5, 'Am i exist?'], 'Am i exist?');
+//
+// Example 2
+// function toFindArr(arr, text) {
+//     arr.find(item => item === text);
+//     console.log(text)
+// }
+// toFindArr(['who knows', 8, 5, 'Am i exist?'], 'Am i exist?');
 
 // 14) greq function vor@ stanum e 2 number ev VERADARCNUM e mek urish function vorn kancheluc heto hashvum e ayd tveri gumar@
 
@@ -294,5 +535,65 @@
 // console.log(makeSum(5,7)());
 
 //====================YOUR ANSWERS=====================
+// ALL ANSWERS IS TRUE
+//Nona answer no check validation
+
+// function returnSum(num1 , num2){
+//     return function (){
+//         return num1 + num2;
+//     }
+// }
+// console.log(returnSum(2,8)());
+
+// Elina answer:
+// no second call
+// function getNumbers(num1, num2){
+//     return getSum () {
+//         return num1+num2;
+//     }
+// }
+// console.log (getNumbers(25, 80));
+
+// Vahag answer:
+// short write return
+
+// function forNumbers(number1, number2) {
+//
+//     function plusNumbers() {
+//         let result = number1 + number2;
+//         console.log(result);
+//
+//     }
+//
+//     return plusNumbers;
+//
+// }
+//
+// forNumbers(88, 55)();
 
 // 15) MIAT IMAST UNECOX INCHVOR OGTAKAR TEKUZ POQR GORC ANOX FUNCTION MTACEQ U INQURUYN GREQ
+// Nona answer
+//  function countDigitsOfNumber(number){
+//      let str = number.toString();
+//      let splitString = str.split("");
+//      let reverseArray = splitString.reverse();
+//      let reversedNum = Number(reverseArray.join(""));
+//      console.log(`The number you have entered has ${str.length} digits.`);
+//      console.log(`Digits of ${number} are:`);
+//      for(let i = 0 ; i < str.length ; i++){
+//          console.log(`${i+1}-th: ${reversedNum%10}`);
+//          reversedNum = (reversedNum - reversedNum%10)/10;
+//      }
+//  }
+//  countDigitsOfNumber(123);
+
+// optimizing
+// function countDigitsOfNumber(number){
+//     let str = number.toString();
+//     console.log(`The number you have entered has ${str.length} digits.`);
+//     console.log(`Digits of ${number} are:`);
+//     for(let i = 0; i < str.length ; i++){
+//         console.log(`${i+1}-th: ${str[i]}`);
+//     }
+// }
+// countDigitsOfNumber(123);
--- lessons-month-1-week-4-day-1/homework/tasks.js
+++ lessons-month-1-week-4-day-1/homework/tasks.js
@@ -1,16 +1,6 @@
 //1) CLOSURI MIJOCOV STACEQ GUMARMAN FUNCTION;
 // aysinqin f(1)(2) ---> ksatananq 3
 
-// RUZANNA ANSWER
-// function f() {
-//     let x = 1;
-//     let y = 2;
-//     return function () {
-//         console.log(x+y)
-//     }
-// }
-// console.log(f(1)(2));
-
 // ====ANSWER=====
 // function sum(a) {
 //     return function (b) {
@@ -24,10 +14,6 @@
 
 // 2) CLOSURI MIJOCOV STEXCEQ FUNCTION VOR@  ERKROD KANCHI ZHAMANAK STACVAC TIV@ KLCNI ARRAY MEJ,
 // ARAJIN KANCHI ZHAMANAK STACVAC TVI CHAPOV;
-
-// RUZANNA NO ANSWER
-//NONA ANSWER
-// Array(arrLength).fill(filledNumber)
 
 // ====ANSWER=====
 // function toArr(a) {
@@ -49,20 +35,8 @@
 // var arr = [];
 // const arr = [];
 
-// RUZANNA ANSWER
-// var-@ tvyal skopi mej hamarvum e global popoxakan,vory entarkvum e hoistingi;
-//NONA ANSWER
-// var is a global variable type, let is a local one.
-
 
 // 4) HAYTARAREQ 3 TESAKI FUNCTION (DECALARTION, EXPRESSION, ARROW FUNCTION) EV NSHEQ TARBERUTYUNNERN
-
-// NONA ANSWER
-// name = function() {
-//     return name;
-// }
-
-//Arrow functions allow us to write function shorter
 
 // 5) INCH KTPI XI?
 // for (let i = 0; i < 5; i++) {
@@ -86,10 +60,6 @@
 //     a += 1;
 // }
 
-//RUZANNA ANSWER
-// qani vor a+=1 havasar e 3 isk menq arden isk unenq a-i arjeq@ , vory havasar e 2;
-// if-ic durs kga kmtni else-i mej ev kartatpi ++a (1+2) vorn el havasar e 3
-
 //=====ANSWER=====
 // 3
 
@@ -105,22 +75,6 @@
 //         console.log(2);
 //     }
 // } while (a < 8);
-
-//RUZANNA ANSWER
-// skzbum kmtni if-i mej kstugi vor paymani erku hatvatsnernel jisht linen ++a (6) bajanats 3 mnacord chi mnum =>true && b(3) bajanats 2 !=== 0 =>folse
-// durs kga if- i mijic kmtni else-i mej kstugi vor paymani gone paymanneric gone meky jisht lini ev qani vor
-// henc arajin paymany jisht e a===5-i ktpi 2
-//2
-// NONA ANSWER
-// var b = 3;
-// let a =5;
-// do {
-//     if (++a % 3 === 0 && b % 2 === 0) { //++a = 6 but b%2 is not 5,so if is false
-//         console.log(1);
-//     } else if (a === 5 || a % 2 === 0) { // a is 5, so else runs and program shows 2
-//         console.log(2);
-//     }
-// } while (a < 8); //5 is < 8 so do runs again
 
 //=====ANSWER=====
 // 2,2
@@ -160,10 +114,6 @@
 // }
 // sayWelcome("Henry")();
 
-// RUZANNA ANSWER
-// Haytararum enq sayWelcome() function ev talis enq name argument@, apa let popoxakanin vor@ uni message
-// arjeqy veragrum enq...
-
 //=====ANSWER=====
 //HELLO HENRY
 
@@ -179,13 +129,13 @@
 //=====ANSWER=====
 // logger()()()
 
-//  RUZANNA
 // 5) greq function vor kstana array vori elementneric gone mekn eli array e ev erku callback;
 //       function@ petq e hashvi bolor tveri gumar@ naev ayn tveri voronq gtnvum en nested arrayi mej ev nested arrayneri qanak@;
 //    arajin calbback petq e tpi patasxan@ ev nshi te qani array type- i element e exel himnakan arrayi mej;
 //     erkrord callback petq e tpi 0 ete vochmi number EV vochmi array type-i element chi exel himanakn arrayum
 //   orinak tvual arrayi depqum--> [5, [4, 'a'], true, ['c', 1, 'b']];
 //  petq e ashxati arajin callback@ ev tpi 10(bolor tveri gumar@) heto 2(array elementeri qanak@)
+
 // function callBack1(sum,num){
 //     console.log(sum,num)
 // }
