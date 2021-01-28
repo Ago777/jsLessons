71176630d519f41b443a0f993908371f90d2bfff
Grigoryan <argishtigrigoryan1991@gmail.com>
2021-01-28 16:11:59 +0400

    answers

---
+++ lessons-month-1-week-4-day-2/homework/tesakan.js
@@ -0,0 +1,59 @@
+// 1. popoxakanneri haytararman dzever@
+
+// 2. dranc tarberutyun@ (hoisting, scope)
+
+// 3. constov haytararvac popoxakanner (vor depqum kareli e poxel)
+
+// 4. js typer@
+
+// 5. inch e NaN@, vor typein e patkanum ev vor depqerum e stacvum NaN
+
+// 6. inch e undefined@ ev null@
+
+// 7.  ==  ev === havasarutyunneri tarberutyun
+
+// 8. looperi tesakner
+
+// 9. functioni haytararman dzever
+
+// 10. dranc tarberutyunner@
+
+// 11. rest ev spread operatorner
+
+// 12. arrayi masin reference type
+
+// 13. inch e irenic nerkayacnum functioni mej return artahayatutyun@ ev dra bacakayutyun@
+
+// 14. hoisting (inch e irenic nerkayacnum ev inchn e entarkvum hoistingi js-um)
+
+// 15. global ev local popoxakanner
+
+// 16. scope. (inch e block scop@, vor popoxakannern en entarkvum block scopi voronq voch)
+
+// 17. inchpes en ashxatum || , && operatornern
+
+// 18. inchpes en ashxatum ++, -- operatornern
+
+// 19. inchpes e ashxatum switch case@
+
+// 20. inchpes aveli karch ogtagorcel if els@ (ternary operator)
+
+// 21.inchpes arrayic jnjel inchvor element
+
+// 22. inchu xorhurd chen trvum shift unshift methodnern
+
+// 23. inchpes datarkel array@
+
+// 24. inch methodnber giteq arrayi het asxhatelu
+
+// 25. inch e closur@
+
+// 26. inch vat koxmer uni
+
+// 27. inch e rekursian (vat koxmer@)
+
+// 28. arrayi haytararman inch dzever giteq
+
+// 29. inchpes stugel popoxaknn arjeqw uni te voch (zhxtman operator)
+
+// 30. iife, self invokig function (inch en sranq ev inchpes een ogtagorcvum)
