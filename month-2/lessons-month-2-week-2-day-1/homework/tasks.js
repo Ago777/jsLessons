f37293861370a1d48b869efb165ef6084457af44
Grigoryan <argishtigrigoryan1991@gmail.com>
2021-02-01 02:42:23 +0400

    answers

---
+++ month-2/lessons-month-2-week-2-day-1/homework/tasks.js
@@ -0,0 +1,19 @@
+// 1) greq function vorn stanum e object ev stugum datark e te voch ---> hampatasxan veradarcneluv true kam false
+
+// 2) greq function vorn stanum e nerqevum nshvac  object@ ev hashvum bolor ashxatoxneri ashxatavardzeri gumar@
+
+// let employeeSalaries = {
+//     Jhon: 100,
+//     Nick: 200,
+//     Zidane: 1000,
+// }
+
+// 3) greq nuyn 2 xndir@ bayc hashvi arnelov vor asxhatavardzern nshvac en stringov orinak Jhon: '100$' (chaseq chhaskaca)
+
+// 4) greq function vorn stanum e object ev veradarcnum mek ayl object vortex klinen naxord objecti number value-neri krknapatiknern
+// orinak ete stacel enq {price: 100, name: 'John'} kveradarcni --> {price: 200, name: 'John'}
+
+// 5) greq function vorn stanum e object ev veradarcnum mek ayl object vortex values keyov klinen stacvac objecti value-nern arrayov
+//    isk keys keyov stacvac objecti keyer@ arrayov,
+//    orinak stanum e {id: 5, name: 'John'} kveradarcni --> {values: [5,'John'], keys: ['id', 'name']} (shat heshta, ogtvel methodneric)
+
