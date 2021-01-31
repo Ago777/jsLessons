e2a1d8c5f840040ab8132cac92d70eedb563f024
Grigoryan <argishtigrigoryan1991@gmail.com>
2021-01-31 16:50:23 +0400

    answers

--- month-2/lessons-month-2-week-1-day-3/loop-for-in-1.js
+++ month-2/lessons-month-2-week-1-day-3/loop-for-in-1.js
@@ -1,94 +1,66 @@
-// =========== JS DATA TYPES=================
+// ===========  Object loop for in ================
 
-// THERE IS 8 DATA TYPES
-
-// number,
-// bigint,
-// string,
-// boolean,
-// symbol --- month 2,
-// object --- month 2,
-// undefined,
-// null
-
-
-// ===========number types all numbers, Infinity -Infinity, NaN=============
-// let x = 5;
-// let y = 2;
+// for(let key in obj) {
 //
-// console.log(x + y);
-// console.log(x - y);
-// console.log(x * y);
-// console.log(x / y);
-// console.log(x % y);
+// }
 
-//Infinity
-// console.log(5 / 0)
-
-//NaN -- Not a Number
-// console.log(7 * 'John');
-
-//==============bigint===============
-// numbers over 9007199254740991;
-
-
-// =================string types ' ', " ", `` ==================;
-
-// let name = 'John';
-// let otherName = "John";
-// let anotherName = `John`;
-
-// -----even if there is a number----
-
-// let x = '555';
-
-//----- with ``---------
-// let name = 'John';
-// let message = `my name is ${name}`;
-// let message = 'my name is ${name}';
-// console.log(message);
-
-//============boolean types===================
-
-// let isChecked = true;
-// let isOld = false;
-
-//---- > < operators;
-// let isGoog = 5 > 1;
-// console.log(isGoog)
-
-//============null===================
-// null is NO VALUE,
-
-// let x = null;
-// let age = 21;
-// age = null;
-// console.log(age)
-
-
-//============undefined===================
-// let age;
-// console.log(age);
-
-// is not defined
-// dont do this;
-// let x = undefined;
-// do this
-// let x = null
-
-
-// ---------these are all primitive types-----------
-
-// =====typeof operator
-
-// let age = 21;
-// console.log(typeof age)
+// let obj = {
+//     id: 5,
+//     name: 'user',
+//     isTest: true
+// }
 //
-// let name = 'John';
-// console.log(typeof name)
+// for (let key in obj) {
+//     // console.log(key);
+//     // console.log(obj[key])
+// }
+
+// ----we can our variable instead key-----
+
+//--------------iteration is not ordered if keys is numbers-------
+
+// let obj = {
+//     '3' : 'c',
+//     '4' : 'd',
+//     '1' : 'a',
+//     '2' : 'b',
+// }
 //
-// let isChecked = true;
-// console.log(typeof isChecked);
+// for (let key in obj) {
+//     // console.log(key);
+// }
+
+// -----for in loop can use for arrays like for of------
+
+// ----examples----
+
+// ----push to array---
+// let obj = {
+//     id: 5,
+//     name: 'user',
+//     isTest: true
+// }
 //
-// let bug = null;
-// console.log(typeof bug)
+// const arr = [];
+//
+// for (let key in obj) {
+//     arr.push(obj[key])
+// }
+//
+// console.log(arr);
+//----------------------------
+//----copy object-----
+// let obj = {
+//     name: 'John',
+//     surname: 'Wick',
+//     age: 30,
+// }
+//
+// let obj2 = {};
+//
+// for(let key in obj) {
+//     obj2[key] = obj[key]
+// }
+//
+// console.log(obj2);
+// console.log(obj2 === obj);
