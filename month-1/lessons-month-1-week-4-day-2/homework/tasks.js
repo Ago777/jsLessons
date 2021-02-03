9c2e136c2b636a90fcea2c4a9abfd48d93d00b64
argishti.grigoryan <argishti.grigoryan@betconstruct.com>
2021-02-03 15:01:30 +0400

    homework

--- month-1/lessons-month-1-week-4-day-2/homework/tasks.js
+++ month-1/lessons-month-1-week-4-day-2/homework/tasks.js
@@ -2,13 +2,26 @@
 // RECURSIAI MIJOCOV AMEN KANCHI ZHAMANAK ARRAYI MEJ LCREQ 1 TIV@ MINCHEV ARRAYI ERKARUTYUN@ DARNA 3
 //
 // ===================ANSWER===================
+// function f(arr, num) {
+//     if(arr.length === 3) return arr;
+//     arr.push(num);
+//     return f(arr, num)
+// }
+//
+// console.log(f([], 5));
+
 
 //2) RECURSIAI MIJOCOV HASHVEQ TRVAC TVI FAKTORIAL@
 // 5 ---> 1 * 2 * 3 * 4 * 5
 
 // ===================ANSWER===================
 
-
+// function factorial(n) {
+//     if(n === 1) return n;
+//     return n * factorial(n-1)
+// }
+//
+// console.log(factorial(5));
 
 
 
