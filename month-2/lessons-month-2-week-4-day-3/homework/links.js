5785af83c2e8bfeac33e707403771d679b5a5592
Grigoryan <argishtigrigoryan1991@gmail.com>
2021-03-12 18:15:01 +0400

    'aaa'

--- month-2/lessons-month-2-week-4-day-3/homework/links.js
+++ month-2/lessons-month-2-week-4-day-3/homework/links.js
@@ -1,14 +1,8 @@
-// global window
-// https://learn.javascript.ru/global-object
-
 // this
 // https://learn.javascript.ru/object-methods
 
 //constructors
 // https://learn.javascript.ru/constructor-new#tasks
-
-// proto
-// https://learn.javascript.ru/prototype-inheritance#tasks
 
 // Object create
 // https://learn.javascript.ru/prototype-methods
