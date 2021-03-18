1d188024920212a3a7b5881e30316b97739dcc2f
Grigoryan <argishtigrigoryan1991@gmail.com>
2021-03-19 02:03:31 +0400

    answers

--- month-3/day-3/homework/task.js
+++ month-3/day-3/homework/task.js
@@ -1,4 +1,3 @@
-// mer studentneri html-ic ogtveq kam greq dzern vonc uzeq
-//1. avelacreq amen studenti vra x knopka vori vra sxmeluc kjnji studentin
-
-//2. greq en orinak vor dzez cuyc em tvel sas-i producti count@
+// ogtvelov mer studnetni htmlic ev arrayic
+// click aneluc nshavc studenti object@ qceq localstorage
+// 2rd angam clikc aneluc jnjeq storagic
